b5bcfc00e47198f90f8e59596251721e95e74be7
Rafal Krupinski <rafiwts@gmail.com>
2023-06-05 22:00:46 +0200

    Adding a path to a new app

--- elearning/elearning/urls.py
+++ elearning/elearning/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('accounts/login/', auth_views.LoginView.as_view(),
@@ -24,5 +25,5 @@ urlpatterns = [
     path('accounts/logout/', auth_views.LogoutView.as_view(),
          name='logout'),
     path('admin/', admin.site.urls),
-   
+    path('course/', include('courses.urls'))
 ]
